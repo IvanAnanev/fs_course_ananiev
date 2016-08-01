474b100ed96cd0ff252586e215b9a0912195c200
Ivan Ananiev <ivan.ananiev@flatstack.com>
2016-08-02 00:04:13 +0300

    [add] flash message with bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,8 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    flash[:notice]  = "Notice"
+    flash[:success] = "Success"
+    flash[:error]   = "Error"
+    flash[:alert]   = "Alert"
+  end
 end
